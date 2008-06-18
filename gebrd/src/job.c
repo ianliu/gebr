5e96936c62afeac09cbe31232a95ff36e8fa5ecc
brauliobo <none@none>
2008-06-18 16:02:30 +0000

    fixed typo

--- gebrd/src/job.c
+++ gebrd/src/job.c
@@ -473,10 +473,11 @@ job_run_flow(struct job * job, struct client * client)
 			g_string_printf(cmd_line, "bash -l -c \"export DISPLAY=%s; %s\"",
 				client->display->str, escaped_str);
 		else
-			g_string_printf(cmd_line, "bash -l -c \"export DISPLAY=127.0.0.1:%s; %s\"",
+			g_string_printf(cmd_line, "bash -l -c \"export DISPLAY=127.0.0.1%s; %s\"",
 				client->display->str, escaped_str);
-	} else
+	} else {
 		g_string_printf(cmd_line, "bash -l -c \"%s\"", escaped_str);
+	}
 	gebrd_message(LOG_DEBUG, "Client '%s' flow about to run: %s",
 		client->protocol->hostname->str, cmd_line->str);
 
