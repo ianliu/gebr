eb603c9c2a03e0e5b8b94116f38922844bf9f124
biloti <none@none>
2009-01-14 19:42:16 +0000

    fix string enconde

--- debr/src/interface.c
+++ debr/src/interface.c
@@ -98,9 +98,9 @@ debr_setup_ui(void)
 	 */
 	gtk_window_set_default_icon(pixmaps_gebr_icon_16x16());
 	debr.window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-	gtk_window_set_title(GTK_WINDOW(debr.window), "DÃ©BR");
+	gtk_window_set_title(GTK_WINDOW(debr.window), "DéBR");
 	gtk_widget_set_size_request(debr.window, 600, 450);
-	debr.about = about_setup_ui("DÃ©BR", _("Flow designer for GÃªBR"));
+	debr.about = about_setup_ui("DéBR", _("Flow designer for GéBR"));
 
 	g_signal_connect(debr.window, "delete_event",
 		GTK_SIGNAL_FUNC(debr_quit), NULL);
