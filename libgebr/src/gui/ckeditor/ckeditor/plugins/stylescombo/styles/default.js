d6365697a9e78f06bf0379daabc9fe105014265d
ian.liu88 <none@none>
2010-02-04 18:54:44 +0000

    Added default styles for editor.

---
+++ libgebr/src/gui/ckeditor/ckeditor/plugins/stylescombo/styles/default.js
@@ -0,0 +1,6 @@
+﻿/*
+Copyright (c) 2003-2010, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+
+CKEDITOR.addStylesSet('default',[{name:'Normal',element:'p',styles:{}},{name:'Section',element:'h2',styles:{}},{name:'Subsection',element:'h3',styles:{}},{name:'Code',element:'pre',styles:{}}]);
