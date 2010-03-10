7e0fcf52751ec9ed83c53b998aa83d39e9c1e6ef
Ian Liu Rodrigues <ian.liu88@gmail.com>
2010-03-10 17:45:04 -0300

    Implemented GebrGuiSaveDialog with custom overwrite confirmation dialog. Need testing.

--- libgebr/src/gui/gui/gebr-gui-save-dialog.c
+++ libgebr/src/gui/gui/gebr-gui-save-dialog.c
@@ -64,8 +64,13 @@ static void gebr_gui_save_dialog_class_init(GebrGuiSaveDialogClass * class)
 							    G_PARAM_READWRITE));
 }
 
-static void gebr_gui_save_dialog_init(GebrGuiSaveDialog * directory_chooser)
+static void gebr_gui_save_dialog_init(GebrGuiSaveDialog * self)
 {
+	gtk_dialog_add_buttons(GTK_DIALOG(self),
+			       GTK_STOCK_CANCEL, GTK_RESPONSE_CANCEL,
+			       GTK_STOCK_SAVE, GTK_RESPONSE_OK,
+			       NULL);
+	gtk_file_chooser_set_do_overwrite_confirmation(GTK_FILE_CHOOSER(self), FALSE);
 }
 
 static void gebr_gui_save_dialog_set_property(GObject *object, guint prop_id, const GValue *value, GParamSpec *pspec)
@@ -100,9 +105,15 @@ static void gebr_gui_save_dialog_get_property(GObject *object, guint prop_id, GV
  * Public functions
  */
 
-GtkWidget *gebr_gui_save_dialog_new()
+GtkWidget *gebr_gui_save_dialog_new(GtkWindow *parent)
 {
-	return g_object_new(GEBR_GUI_TYPE_SAVE_DIALOG, NULL);
+	GtkWidget *self = g_object_new(GEBR_GUI_TYPE_SAVE_DIALOG, NULL);
+
+	gtk_window_set_modal(GTK_WINDOW(self), TRUE);
+	if (parent)
+		gtk_window_set_destroy_with_parent(GTK_WINDOW(self), parent);
+
+	return self;
 }
 
 void gebr_gui_save_dialog_set_default_extension(GebrGuiSaveDialog *self, const gchar *extension)
@@ -119,6 +130,55 @@ const gchar *gebr_gui_save_dialog_get_default_extension(GebrGuiSaveDialog *self)
 
 gchar *gebr_gui_save_dialog_run(GebrGuiSaveDialog *self)
 {
-	return NULL;
+	GtkWidget *dialog;
+	GString *filename;
+	gboolean free_str = TRUE;
+
+	filename = g_string_new(NULL);
+
+	while (TRUE) {
+		gchar *tmp;
+
+		if (gtk_dialog_run(GTK_DIALOG(self)) != GTK_RESPONSE_OK)
+			break;
+
+		tmp = gtk_file_chooser_get_filename(GTK_FILE_CHOOSER(self));
+		g_string_assign(filename, tmp);
+		g_free(tmp);
+
+		if (self->extension && !g_str_has_suffix(tmp, self->extension))
+			g_string_append(filename, self->extension);
+
+		if (g_file_test(filename->str, G_FILE_TEST_EXISTS)) {
+			gchar *basename;
+			basename = g_path_get_basename(filename->str);
+			dialog = gtk_message_dialog_new_with_markup(GTK_WINDOW(self),
+								    GTK_DIALOG_MODAL | GTK_DIALOG_DESTROY_WITH_PARENT,
+								    GTK_MESSAGE_QUESTION,
+								    GTK_BUTTONS_NONE,
+								    _("<b>A file named \"%s\" already exists. "
+								      "Do you want to replace it?</b>"), basename);
+			gtk_dialog_add_buttons(GTK_DIALOG(dialog),
+					       GTK_STOCK_CANCEL, GTK_RESPONSE_CANCEL,
+					       _("_Replace"), GTK_RESPONSE_OK,
+					       NULL);
+			gtk_message_dialog_format_secondary_text(GTK_MESSAGE_DIALOG(dialog),
+								 _("The file you are saving already exists. "
+								   "Replacing it will overwrite its contents."));
+
+			g_free(basename);
+			if (gtk_dialog_run(GTK_DIALOG(dialog)) == GTK_RESPONSE_OK) {
+				free_str = FALSE;
+				break;
+			}
+		} else {
+			free_str = FALSE;
+			break;
+		}
+	}
+
+	gtk_widget_destroy(GTK_WIDGET(self));
+
+	return g_string_free(filename, free_str);
 }
 
