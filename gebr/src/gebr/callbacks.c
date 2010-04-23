d7d0cdf9e09b850fe3da484ab5d4e94ae291060c
Braulio Barros de Oliveira <brauliobo@gebrproject.com>
2010-04-23 16:30:50 -0300

    fixed documentation for callbacks.{c,h}

--- gebr/src/gebr/callbacks.c
+++ gebr/src/gebr/callbacks.c
@@ -36,9 +36,6 @@
 #include "ui_job_control.h"
 #include "ui_help.h"
 
-/**
- * \internal
- */
 void on_new_activate(void)
 {
 	switch (gtk_notebook_get_current_page(GTK_NOTEBOOK(gebr.notebook))) {
@@ -50,9 +47,6 @@ void on_new_activate(void)
 	}
 }
 
-/**
- * \internal
- */
 void on_new_project_activate(void)
 {
 	switch (gtk_notebook_get_current_page(GTK_NOTEBOOK(gebr.notebook))) {
@@ -64,9 +58,6 @@ void on_new_project_activate(void)
 	}
 }
 
-/**
- * \internal
- */
 void on_new_line_activate(void)
 {
 	switch (gtk_notebook_get_current_page(GTK_NOTEBOOK(gebr.notebook))) {
@@ -78,9 +69,6 @@ void on_new_line_activate(void)
 	}
 }
 
-/**
- * Select copy target depending on the context
- */
 void on_copy_activate(void)
 {
 	switch (gtk_notebook_get_current_page(GTK_NOTEBOOK(gebr.notebook))) {
@@ -95,9 +83,6 @@ void on_copy_activate(void)
 	}
 }
 
-/**
- * Select paste target depending on the context
- */
 void on_paste_activate(void)
 {
 	switch (gtk_notebook_get_current_page(GTK_NOTEBOOK(gebr.notebook))) {
@@ -112,33 +97,21 @@ void on_paste_activate(void)
 	}
 }
 
-/** 
- * Call <gebr_quit>
- */
 void on_quit_activate(void)
 {
 	gebr_quit();
 }
 
-/** 
- * Call <document_properties_setup_ui>
- */
 gboolean on_document_properties_activate(void)
 {
 	return document_properties_setup_ui(document_get_current());
 }
 
-/** 
- * Call <document_dict_edit_setup_ui>
- */
 void on_document_dict_edit_activate(void)
 {
 	document_dict_edit_setup_ui();
 }
 
-/**
- * Call <project_delete> from <project.c>
- */
 void on_project_line_delete_activate(void)
 {
 	if (gebr_geoxml_document_get_type(gebr.project_line) == GEBR_GEOXML_DOCUMENT_TYPE_PROJECT)
@@ -147,139 +120,88 @@ void on_project_line_delete_activate(void)
 		line_delete(TRUE);
 }
 
-/**
- * *Fill me in!*
- */
 void on_project_line_paths_activate(void)
 {
 	path_list_setup_ui();
 }
 
-/**
- * Call <project_line_import> from <ui_project_line.c>
- */
 void on_project_line_import_activate(void)
 {
 	project_line_import();
 }
 
-/**
- * Call <project_line_export> from <ui_project_line.c>
- */
 void on_project_line_export_activate(void)
 {
 	project_line_export();
 }
 
-/**
- * Call <flow_import> from <flow.c>
- */
 void on_flow_import_activate(void)
 {
 	flow_import();
 }
 
-/**
- * Call <flow_export> from <flow.c>
- */
 void on_flow_export_activate(void)
 {
 	flow_export();
 }
 
-/**
- * Call <flow_export_as_menu> from <flow.c>
- */
 void on_flow_export_as_menu_activate(void)
 {
 	flow_export_as_menu();
 }
 
-/**
- * Call <flow_delete> from <flow.c>
- */
 void on_flow_delete_activate(void)
 {
 	flow_delete(TRUE);
 }
 
-/** 
- * Adjust selection and show flow IO dialog
- */
 void on_flow_io_activate(void)
 {
 	flow_browse_single_selection();
 	flow_io_setup_ui(TRUE);
 }
 
-/**
- * Call <flow_io_setup_ui> from <flow.c>
- *
- */
 void on_flow_execute_activate(void)
 {
 	flow_fast_run();
 }
 
-/** 
- * Call <flow_revision_save>
- */
 void on_flow_revision_save_activate(void)
 {
 	flow_revision_save();
 }
 
-/** 
- * Call <flow_browse_single_selection>
- */
 void on_flow_revision_show_menu(void)
 {
 	flow_browse_single_selection();
 }
 
-/**
- */
 void on_flow_component_help_activate(void)
 {
 	program_help_show();
 }
 
-/**
- * Call <flow_program_remove>
- */
 void on_flow_component_delete_activate(void)
 {
 	flow_program_remove();
 }
 
-/**
- * *Fill me in!*
- */
 void on_flow_component_properties_activate(void)
 {
 	flow_edition_component_activated();
 }
 
-/**
- * *Fill me in!*
- */
 void on_flow_component_refresh_activate(void)
 {
 	menu_list_create_index();
 	menu_list_populate();
 }
 
-/* Function: on_flow_component_status_activate
- * Call <flow_edition_status_changed>
- */
 void on_flow_component_status_activate(GtkRadioAction * action, GtkRadioAction * current)
 {
 	flow_edition_status_changed();
 }
 
-/* Function: on_job_control_save
- * Call <job_control_save>
- */
 void on_job_control_save(void)
 {
 	job_control_save();
@@ -300,37 +222,21 @@ void on_job_control_clear(void)
 	job_control_clear(FALSE);
 }
 
-/*
- * Function: on_job_control_stop
- * Call <job_control_stop>
- *
- */
 void on_job_control_stop(void)
 {
 	job_control_stop();
 }
 
-/*
- * Function: on_configure_preferences_activate
- * *Fill me in!*
- *
- */
 void on_configure_preferences_activate(void)
 {
 	preferences_setup_ui(FALSE);
 }
 
-/* Function: on_configure_servers_activate
- * Show servers's configuration dialog
- */
 void on_configure_servers_activate(void)
 {
 	server_list_show(gebr.ui_server_list);
 }
 
-/* Function: on_help_contents_activate
- * *Fill me in!*
- */
 void on_help_contents_activate(void)
 {
 	GString *html_path = g_string_new("");
@@ -339,18 +245,11 @@ void on_help_contents_activate(void)
 	g_string_free(html_path, TRUE);
 }
 
-/* Function: on_help_about_activate
- * Show about dialog
- */
 void on_help_about_activate(void)
 {
 	gtk_widget_show_all(gebr.about.dialog);
 }
 
-/*
- * Function: navigation_bar_update
- * 
- */
 void navigation_bar_update(void)
 {
 	GString *markup;
--- gebr/src/gebr/callbacks.h
+++ gebr/src/gebr/callbacks.h
@@ -20,49 +20,150 @@
 
 #include <gtk/gtk.h>
 
+/**
+ *
+ */
 void on_new_activate(void);
+/**
+ *
+ */
 void on_new_project_activate(void);
+/**
+ *
+ */
 void on_new_line_activate(void);
+/**
+ * Select copy target depending on the context
+ */
 void on_copy_activate(void);
+/**
+ * Select paste target depending on the context
+ */
 void on_paste_activate(void);
+/** 
+ * Call #gebr_quit
+ */
 void on_quit_activate(void);
 
+/** 
+ * Call #document_properties_setup_ui
+ */
 gboolean on_document_properties_activate(void);
+/** 
+ * Call #document_dict_edit_setup_ui
+ */
 void on_document_dict_edit_activate(void);
 
+/**
+ * Call #project_delete> from #project.c
+ */
 void on_project_line_delete_activate(void);
+/**
+ *
+ */
 void on_project_line_paths_activate(void);
+/**
+ * Call #project_line_import> from #ui_project_line.c
+ */
 void on_project_line_import_activate(void);
+/**
+ * Call #project_line_export> from #ui_project_line.c
+ */
 void on_project_line_export_activate(void);
 
+/**
+ * Call #flow_import> from #flow.c
+ */
 void on_flow_import_activate(void);
+/**
+ * Call #flow_export> from #flow.c
+ */
 void on_flow_export_activate(void);
+/**
+ * Call #flow_export_as_menu> from #flow.c
+ */
 void on_flow_export_as_menu_activate(void);
+/**
+ * Call #flow_delete> from #flow.c
+ */
 void on_flow_delete_activate(void);
+/** 
+ * Adjust selection and show flow IO dialog
+ */
 void on_flow_io_activate(void);
+/**
+ * Call #flow_io_setup_ui> from #flow.c
+ */
 void on_flow_execute_activate(void);
+/** 
+ * Call #flow_revision_save
+ */
 void on_flow_revision_save_activate(void);
+/** 
+ * Call #flow_browse_single_selection
+ */
 void on_flow_revision_show_menu(void);
 
 void on_flow_component_help_activate(void);
+/**
+ * Call #flow_program_remove
+ */
 void on_flow_component_delete_activate(void);
+/**
+ *
+ */
 void on_flow_component_properties_activate(void);
+/**
+ *
+ */
 void on_flow_component_refresh_activate(void);
+/**
+ * Call #flow_edition_status_changed
+ */
 void on_flow_component_status_activate(GtkRadioAction * action, GtkRadioAction * current);
-void on_flow_component_refresh_activate(void);
 
+/**
+ * Call #job_control_save
+ */
 void on_job_control_save(void);
+/**
+ *
+ */
 void on_job_control_cancel(void);
+/**
+ *
+ */
 void on_job_control_close(void);
+/**
+ *
+ */
 void on_job_control_clear(void);
+/**
+ * Call #job_control_stop
+ */
 void on_job_control_stop(void);
 
+/**
+ *
+ */
 void on_configure_preferences_activate(void);
+/**
+ * Show servers's configuration dialog
+ */
 void on_configure_servers_activate(void);
 
+/**
+ *
+ */
 void on_help_contents_activate(void);
+/**
+ * Show about dialog
+ */
 void on_help_about_activate(void);
 
+/**
+ *
+ */
 void navigation_bar_update(void);
 
 #endif				//__CALLBACKS_H
