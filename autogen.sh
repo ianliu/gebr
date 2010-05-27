4dfb291d17c3ce8434883a5718141678e9cce8af
Ian Liu Rodrigues <ian.liu88@gmail.com>
2010-05-27 09:53:16 -0300

    Adjusting autotool files for GtkDoc
    Updates issue 389
    
    To generate the GtkDoc for the API, on must run ./configure --enable-gtk-doc
    and compile the code, but, that is not working by now. I am getting an error
    saying "Undefined reference to `sexy_icon_entry_get_type'". See the
    Makefile.am in libgebr/docs/reference/gui to peek at the compilation and
    linking flags.
    
    http://code.google.com/p/gebr/source/browse/libgebr/docs/reference/gui/Makefile.am#81

--- autogen.sh
+++ autogen.sh
@@ -1,2 +1,11 @@
-for sub in libgebr gebrd gebr debr; do cp -a configure.ac.decl Makefile.decl $sub; done
+for sub in libgebr gebrd gebr debr;
+do
+	cp -a configure.ac.decl Makefile.decl $sub;
+done
+
+##
+# Generates gtk-doc.make file
+#
+cd libgebr; gtkdocize || exit 1; cd ..
+
 AUTOPOINT='intltoolize --automake --copy' autoreconf --install $*
