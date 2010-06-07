d7f1777107bcbef1686c91947d1d1c95e3daab79
fmatheus <none@none>
2010-06-07 17:07:14 -0300

    Sample gtk+test using about dialog.

--- gebr/src/gebr/tests/test-help.c
+++ gebr/src/gebr/tests/test-help.c
@@ -15,6 +15,7 @@
  *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <gdk/gdkkeysyms.h>
 #include <gtk/gtk.h>
 
 #include <libgebr/intl.h>
@@ -24,28 +25,38 @@
 #include <../defines.h>
 #include <interface.h>
 
-static void
-test_gebr_help_ckeditor_confirm_save(void)
-{
-
+int gtk_loop() {
+	while (gtk_events_pending()) {
+		gtk_main_iteration();
+	}
+	sleep(0);
 }
 
-int main(int argc, char *argv[])
-{
-	int ret;
+static void test_gebr_help_about(void) {
+	GtkWidget *about = gebr_gui_about_setup_ui("gebr", NULL).dialog;
+	gtk_widget_show(about);
+	gtk_loop();
+	GtkWidget *ok = gtk_test_find_widget (about, "*Close*", GTK_TYPE_BUTTON);
+	gboolean hit = gtk_test_widget_send_key (ok, GDK_Return, 0);
+	g_assert (hit == TRUE);
+	gtk_loop();
+	gtk_widget_show(about);
+	gtk_loop();
+	gboolean click = gtk_test_widget_click(ok, 1, 0);
+	g_assert (click == TRUE);
+	gtk_loop();
+}
+
+static void test_gebr_help_ckeditor_confirm_save(void) {
+}
+
+int main(int argc, char *argv[]) {
 
 	/* initialization */
 	gtk_test_init(&argc, &argv, NULL);
 
-	gebr_gui_setup_theme();
-	gebr_gui_setup_icons();
-	gebr_setup_ui();
-	gebr_init();
-
-	gtk_main();
-
+	g_test_add_func("/gebr/help/about", test_gebr_help_ckeditor_confirm_save);
 	g_test_add_func("/gebr/help/ckeditor/confirm-save", test_gebr_help_ckeditor_confirm_save);
-	ret = g_test_run();
+	return g_test_run();
 
-	return ret;
 }
