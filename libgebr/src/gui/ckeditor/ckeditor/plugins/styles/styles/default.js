1a5643d65100a137af9b3a419f52b82c84e7bed7
Ricardo Biloti <biloti@gebrproject.com>
2010-07-28 21:31:38 -0300

    Required parameter style added do ckeditor

--- libgebr/src/gui/ckeditor/ckeditor/plugins/styles/styles/default.js
+++ libgebr/src/gui/ckeditor/ckeditor/plugins/styles/styles/default.js
@@ -6,7 +6,9 @@ var styles = [{name:'Normal',element:'p'}];
 if (!menu_edition)
 	styles.push({name:'Title',element:'h1'});
 styles.push({name:'Section',element:'h2'},{name:'Subsection',element:'h3'});
-if (menu_edition)
-	styles.push({name:'Parameter',element:'span',attributes:{'class':'label'}});
+if (menu_edition){
+	styles.push({name:'Optional parameter',element:'span',attributes:{'class':'label'}});
+	styles.push({name:'Required parameter',element:'span',attributes:{'class':'reqlabel'}});
+}
 styles.push({name:'Code',element:'pre'});
 CKEDITOR.stylesSet.add('default', styles);
