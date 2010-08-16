14058f842324e8010fa81b95790f3268291de5be
Ian Liu Rodrigues <ian.liu88@gmail.com>
2010-08-16 11:07:42 -0300

    Rolling back to old 'default.js' style
    --HG--
    branch : help-edit

--- libgebr/src/gui/ckeditor/ckeditor/plugins/styles/styles/default.js
+++ libgebr/src/gui/ckeditor/ckeditor/plugins/styles/styles/default.js
@@ -1,10 +1,14 @@
 ﻿var styles = [{name:'Normal',element:'p'}];
-if (typeof(menu_edition) == 'undefined')
+
+if (menu_edition == false)
 	styles.push({name:'Title',element:'h1'});
+
 styles.push({name:'Section',element:'h2'},{name:'Subsection',element:'h3'});
-if (typeof(menu_edition) != 'undefined') {
+
+if (menu_edition == true) {
 	styles.push({name:'Optional parameter',element:'span',attributes:{'class':'label'}});
 	styles.push({name:'Required parameter',element:'span',attributes:{'class':'reqlabel'}});
 }
+
 styles.push({name:'Code',element:'pre'});
 CKEDITOR.stylesSet.add('default', styles);
