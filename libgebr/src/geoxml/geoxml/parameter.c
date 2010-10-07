9da29222f23d073bf5b40c33117f6cb375ba4f34
Ian Liu Rodrigues <ian.liu88@gmail.com>
2010-10-07 17:51:33 -0300

    Fixed a bug with promotion of DTD 0.3.5 to 0.3.6
    --HG--
    branch : 0.9.18

--- libgebr/src/geoxml/geoxml/parameter.c
+++ libgebr/src/geoxml/geoxml/parameter.c
@@ -91,6 +91,31 @@ __gebr_geoxml_parameter_get_type(GebrGeoXmlParameter * parameter, gboolean resol
 	return GEBR_GEOXML_PARAMETER_TYPE_UNKNOWN;
 }
 
+void __gebr_geoxml_parameter_set_be_reference_with_value(GebrGeoXmlParameter * parameter)
+{
+	GString *value;
+	GString *default_value;
+	GdomeElement *type_element;
+	GebrGeoXmlParameterType type;
+	GebrGeoXmlProgramParameter *program;
+
+	type = __gebr_geoxml_parameter_get_type(parameter, FALSE);
+
+	g_return_if_fail(type != GEBR_GEOXML_PARAMETER_TYPE_REFERENCE);
+
+	program = GEBR_GEOXML_PROGRAM_PARAMETER(parameter);
+	default_value = gebr_geoxml_program_parameter_get_string_value(program, TRUE);
+	value = gebr_geoxml_program_parameter_get_string_value(program, FALSE);
+	type_element = __gebr_geoxml_parameter_get_type_element(parameter);
+
+	gdome_el_removeChild((GdomeElement *) parameter, (GdomeNode *) type_element, &exception);
+	__gebr_geoxml_parameter_insert_type(parameter, GEBR_GEOXML_PARAMETER_TYPE_REFERENCE);
+	gebr_geoxml_program_parameter_set_string_value(program, TRUE, default_value->str);
+	gebr_geoxml_program_parameter_set_string_value(program, FALSE, value->str);
+
+	g_string_free(default_value, TRUE);
+}
+
 void __gebr_geoxml_parameter_set_be_reference(GebrGeoXmlParameter * parameter)
 {
 	GString *default_value;
