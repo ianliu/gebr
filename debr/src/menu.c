e2ec89f0ff01e312f8a961d4a034bd5e20992fb0
Ian Liu Rodrigues <ian.liu88@gmail.com>
2010-10-25 10:31:55 -0200

    Added an error dialog when trying to install an invalid menu

--- debr/src/menu.c
+++ debr/src/menu.c
@@ -86,6 +86,15 @@ static void debr_menu_backup_help(GtkTreeIter * iter);
 
 static void debr_menu_sync_revert_buttons(GtkTreeIter * iter);
 
+static gint debr_menu_check_valid (GebrGeoXmlFlow *menu);
+
+typedef enum {
+	MENU_VALIDATE_INSTALL_CATEGORIES	= 1 << 0,
+	MENU_VALIDATE_INSTALL_PROGRAMS		= 1 << 1,
+	MENU_VALIDATE_INSTALL_TITLE		= 1 << 2,
+	MENU_VALIDATE_INSTALL_PROGRAM_TITLE	= 1 << 3,
+} MenuValidateInstall;
+
 /*
  * Public functions
  */
@@ -707,13 +716,14 @@ void menu_install(void)
 			dialog = gtk_message_dialog_new(GTK_WINDOW(debr.window),
 							(GtkDialogFlags)(GTK_DIALOG_MODAL | GTK_DIALOG_DESTROY_WITH_PARENT),
 							GTK_MESSAGE_WARNING, GTK_BUTTONS_OK,
-							_("There are menus unsaved. You need to "
+							_("There are unsaved menus. You need to "
 							  "save then before install."));
 			gtk_dialog_run(GTK_DIALOG(dialog));
 			gtk_widget_destroy(dialog);
 			return;
 		}
 	}
+
 	gebr_gui_gtk_tree_view_foreach_selected(&iter, debr.ui_menu.tree_view) {
 		GtkWidget *dialog;
 
@@ -723,9 +733,34 @@ void menu_install(void)
 		GebrGeoXmlFlow *menu;
 		MenuStatus status;
 		gboolean do_save = FALSE;
+		gint flags;
 
 		gtk_tree_model_get(GTK_TREE_MODEL(debr.ui_menu.model), &iter,
 				   MENU_STATUS, &status, MENU_PATH, &menu_path, MENU_XMLPOINTER, &menu, -1);
+
+		flags = debr_menu_check_valid (menu);
+		if (flags != 0) {
+			GString *problems = g_string_new ("");
+
+			if (flags & MENU_VALIDATE_INSTALL_CATEGORIES)
+				g_string_append (problems, _("\n - No categories set"));
+
+			if (flags & MENU_VALIDATE_INSTALL_PROGRAMS)
+				g_string_append (problems, _("\n - Menu with no programs"));
+
+			if (flags & MENU_VALIDATE_INSTALL_TITLE)
+				g_string_append (problems, _("\n - Menu has no title"));
+
+			if (flags & MENU_VALIDATE_INSTALL_PROGRAM_TITLE)
+				g_string_append (problems, _("\n - At least one program has no title"));
+
+			gebr_gui_message_dialog (GTK_MESSAGE_ERROR, GTK_BUTTONS_OK,
+						 _("Unable to install menu"),
+						 _("The menu has the following problems, and can not be installed:"
+						   "%s"), problems->str);
+			g_string_free (problems, TRUE);
+			continue;
+		}
 
 		const gchar *menu_filename = gebr_geoxml_document_get_filename(GEBR_GEOXML_DOCUMENT(menu));
 		destination = g_string_new(NULL);
@@ -2337,4 +2372,31 @@ gboolean debr_menu_get_iter_from_xmlpointer (gpointer menu, GtkTreeIter * iter)
 	}
 
 	return FALSE;
+}
+
+static gint debr_menu_check_valid (GebrGeoXmlFlow *menu)
+{
+	GebrGeoXmlSequence *program;
+	MenuValidateInstall flags = 0;
+
+	if (gebr_geoxml_flow_get_categories_number (menu) == 0)
+		flags |= MENU_VALIDATE_INSTALL_CATEGORIES;
+
+	if (gebr_geoxml_flow_get_programs_number (menu) == 0)
+		flags |= MENU_VALIDATE_INSTALL_PROGRAMS;
+
+	if (strlen (gebr_geoxml_document_get_title (GEBR_GEOXML_DOCUMENT (menu))) == 0)
+		flags |= MENU_VALIDATE_INSTALL_TITLE;
+
+	gebr_geoxml_flow_get_program (menu, &program, 0);
+	while (program) {
+		if (strlen (gebr_geoxml_program_get_title (GEBR_GEOXML_PROGRAM (program))) == 0) {
+			flags |= MENU_VALIDATE_INSTALL_PROGRAM_TITLE;
+			break;
+		}
+
+		gebr_geoxml_sequence_next (&program);
+	}
+
+	return flags;
 }
