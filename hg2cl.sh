ac8a3591ea933aa5a4823b8a4670cd0b91e2f8fa
fmatheus <none@none>
2010-11-09 22:33:09 -0200

    Updated ChangeLogs.
    --HG--
    branch : 0.9.18

--- hg2cl.sh
+++ hg2cl.sh
@@ -1,7 +1,8 @@
 hg --quiet log -M --style=changelog $* | perl -pe '
-s/  brauliobo$/ Braulio Barros de Oliveira <brauliobo\@gebrproject.com>/;
-s/  (ian|ianliu)$/  Ian Liu Rodrigues <ian.liu\@gebrproject.com>/;
-s/  biloti$/  Ricardo Biloti <biloti\@gebrproject.com>/;
-s/  fmatheus$/  Fabrício Matheus Gonçalvez <fmatheus\@gebrproject.com>/;
-s/  abaaklini$/  Alexandre Baaklini Gomes Coelho <abaaklini\@gebrproject.com>/;
-s/  (fabioaz|fabiosazevedo)$/  Fabio de Souza Azevedo <fabioaz\@gebrproject.com>/'
+s/  brauliobo.*>$/  Braulio Barros de Oliveira  <brauliobo\@gebrproject.com>/;
+s/  (ian|ianliu).*>$/  Ian Liu Rodrigues  <ian.liu\@gebrproject.com>/i;
+s/  biloti.*>$/  Ricardo Biloti  <biloti\@gebrproject.com>/;
+s/  fmatheus.*>$/  Fabrício Matheus Gonçalvez  <fmatheus\@gebrproject.com>/;
+s/  abaaklini.*>$/  Alexandre Baaklini Gomes Coelho  <abaaklini\@gebrproject.com>/;
+s/  rodrigo.*>$/  Rodrigo Morelatto  <morelatto\@gebrproject.com>/i;
+s/  (fabioaz|fabiosazevedo).*>$/  Fabio de Souza Azevedo  <fabioaz\@gebrproject.com>/'
