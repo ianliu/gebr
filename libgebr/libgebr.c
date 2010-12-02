3950d4a4b116eb6df86ab45ff58e4e75f1ca732f
Ian Liu Rodrigues <ian.liu@gebrproject.com>
2010-12-02 14:48:56 +0200

    Fixed a bug with i18n

--- libgebr/libgebr.c
+++ libgebr/libgebr.c
@@ -19,12 +19,18 @@
 #include <stdlib.h>
 #include <libintl.h>
 #include <locale.h>
+#include <string.h>
 
 #include "libgebr.h"
 #include "defines.h"
 
 void gebr_libinit(const gchar * gettext_package, const gchar * argv0)
 {
+	g_return_if_fail (gettext_package != NULL);
+
+	if (strcmp (gettext_package, "libgebr") != 0)
+		bindtextdomain("libgebr", PACKAGE_LOCALE_DIR);
+
 	bindtextdomain(gettext_package, PACKAGE_LOCALE_DIR);
 	bind_textdomain_codeset(gettext_package, "UTF-8");
 	textdomain(gettext_package);
