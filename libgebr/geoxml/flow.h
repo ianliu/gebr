ecbfbc98407e0d03d9d85571805aa725aa9946ba
Ian Liu Rodrigues <ian.liu88@gmail.com>
2011-02-18 04:22:30 -0200

    Removed old 'servers' API and fixed IO functions
    --HG--
    branch : line-groups-dtd

--- libgebr/geoxml/flow.h
+++ libgebr/geoxml/flow.h
@@ -85,11 +85,6 @@ G_BEGIN_DECLS
 #define GEBR_GEOXML_REVISION(seq) ((GebrGeoXmlRevision*)(seq))
 
 /**
- * Cast from GebrGeoXmlSequence at \p seq to GebrGeoXmlFlowServer
- */
-#define GEBR_GEOXML_FLOW_SERVER(seq) ((GebrGeoXmlFlowServer*)(seq))
-
-/**
  * The GebrGeoXmlFlow struct contains private data only, and should be accessed using the functions below.
  */
 typedef struct gebr_geoxml_flow GebrGeoXmlFlow;
@@ -116,11 +111,6 @@ typedef struct gebr_geoxml_category GebrGeoXmlCategory;
  * The GebrGeoXmlRevision struct contains private data only, and should be accessed using the functions below.
  */
 typedef struct gebr_geoxml_revision GebrGeoXmlRevision;
-
-/**
- * The GebrGeoXmlFlowServer struct contains private data only, and should be accessed using the functions below.
- */
-typedef struct gebr_geoxml_flow_server GebrGeoXmlFlowServer;
 
 #include "program.h"
 #include "macros.h"
@@ -168,133 +158,29 @@ void gebr_geoxml_flow_set_date_last_run(GebrGeoXmlFlow * flow, const gchar * las
 const gchar *gebr_geoxml_flow_get_date_last_run(GebrGeoXmlFlow * flow);
 
 /**
- * Creates a new server as a child of servers tag.
- *
- * \see gebr_geoxml_flow_new_server
- */
-GebrGeoXmlFlowServer *gebr_geoxml_flow_append_server(GebrGeoXmlFlow * flow);
-
-/**
- * Writes into server the ieth server from 'servers' tag.
- * If an error ocurred, the content of server is assigned to NULL.
- * If flow is NULL nothing is done.
- * Returns one of: GEBR_GEOXML_RETV_SUCCESS, GEBR_GEOXML_RETV_INVALID_INDEX, GEBR_GEOXML_RETV_NULL_PTR
- */
-int gebr_geoxml_flow_get_server(GebrGeoXmlFlow * flow, GebrGeoXmlSequence ** server, gulong index);
-
-/**
  * Sets the address of this flow server.
  */
-void gebr_geoxml_flow_server_set_address(GebrGeoXmlFlowServer * server, const gchar * address);
+void gebr_geoxml_flow_server_set_address(GebrGeoXmlFlow *flow, const gchar * address);
 
 /**
  * Returns the address of this flow server.
  * The string must be freed with g_free() .
  */
-const gchar *gebr_geoxml_flow_server_get_address(GebrGeoXmlFlowServer * server);
-
-/**
- * Gets the last ran server.
- */
-GebrGeoXmlFlowServer *gebr_geoxml_flow_servers_get_last_run(GebrGeoXmlFlow * flow);
-
-/**
- * Returns the GebrGeoXmlFlowServer that has \p address
- * \p input, \p output and/or \p error may be NULL which causes them to be ignored
- * If not found returns NULL.
- *
- * If \p flow or \p address is NULL returns NULL.
- */
-GebrGeoXmlFlowServer *gebr_geoxml_flow_servers_query(GebrGeoXmlFlow * flow, const gchar * address,
-						     const gchar * input, const gchar * output, const gchar * error);
-
-/**
- * Get the number of servers \p flow has.
- *
- * If \p flow is NULL returns -1.
- */
-glong gebr_geoxml_flow_get_servers_number(GebrGeoXmlFlow * flow);
-
-/**
- * Set the flow \p server input file path to \p input.
- *
- * If \p server or \p input is NULL nothing is done.
- *
- * \see gebr_geoxml_flow_server_io_get_input gebr_geoxml_flow_io_get_input
- */
-void gebr_geoxml_flow_server_io_set_input(GebrGeoXmlFlowServer * server, const gchar * input);
-
-/**
- * Set the flow \p server output file path to \p output.
- *
- * If \p server or \p output is NULL nothing is done.
- *
- * \see gebr_geoxml_flow_server_io_get_output gebr_geoxml_flow_io_get_output
- */
-void gebr_geoxml_flow_server_io_set_output(GebrGeoXmlFlowServer * server, const gchar * output);
-
-/**
- * Set the \p server error file path to \p error.
- *
- * If \p server or \p error is NULL nothing is done.
- *
- * \see gebr_geoxml_flow_server_io_get_error gebr_geoxml_flow_io_get_error gebr_geoxml_program_set_stderr
- */
-void gebr_geoxml_flow_server_io_set_error(GebrGeoXmlFlowServer * server, const gchar * error);
-
-/**
- * Retrieves the input file path of \p server.
- *
- * If \p server is NULL returns NULL.
- *
- * \see gebr_geoxml_flow_server_io_set_input
- */
-const gchar *gebr_geoxml_flow_server_io_get_input(GebrGeoXmlFlowServer * server);
-
-/**
- * Retrieves the output file path of \p server.
- *
- * If \p server is NULL returns NULL.
- *
- * \see gebr_geoxml_flow_io_set_output
- */
-const gchar *gebr_geoxml_flow_server_io_get_output(GebrGeoXmlFlowServer * server);
-
-/**
- * Retrieves the error file path of \p server.
- *
- * If \p server is NULL returns NULL.
- *
- * \see gebr_geoxml_flow_server_io_set_error
- */
-const gchar *gebr_geoxml_flow_server_io_get_error(GebrGeoXmlFlowServer * server);
+const gchar *gebr_geoxml_flow_server_get_address(GebrGeoXmlFlow *flow);
 
 /**
  * Sets the last run date for this server.
  *
  * \see gebr_geoxml_flow_server_get_date_last_run
  */
-void gebr_geoxml_flow_server_set_date_last_run(GebrGeoXmlFlowServer * server, const gchar * date);
+void gebr_geoxml_flow_server_set_date_last_run(GebrGeoXmlFlow *flow, const gchar * date);
 
 /**
  * Retrieve the last run date for this server.
  *
  * \see gebr_geoxml_flow_server_get_date_last_run
  */
-const gchar *gebr_geoxml_flow_server_get_date_last_run(GebrGeoXmlFlowServer * server);
-
-/**
- * Retrieve from the list of servers the server which
- * has the newest the last run date.
- */
-GebrGeoXmlFlowServer *gebr_geoxml_flow_server_get_last_runned_server(GebrGeoXmlFlow * flow);
-
-/**
- * Sets the flow io tag from the corresponding server tag.
- * This is done by copying all values from the server io tag
- * to the flow io.
- */
-void gebr_geoxml_flow_io_set_from_server(GebrGeoXmlFlow * flow, GebrGeoXmlFlowServer * server);
+const gchar *gebr_geoxml_flow_server_get_date_last_run(GebrGeoXmlFlow *flow);
 
 /**
  * Retrieves the \p server input file path*
