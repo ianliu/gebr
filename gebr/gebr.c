45431034b0786953fd06a72c189bdcdbc854d35b
Ian Liu Rodrigues <ian.liu@gebrproject.com>
2011-03-11 15:37:14 -0300

    Initialize servers_filter model before selecting Line/Flow

--- gebr/gebr.c
+++ gebr/gebr.c
@@ -118,18 +118,6 @@ void gebr_init(void)
 	/* finally the config. file */
 	gebr_config_load();
 
-	gebr.ui_project_line->servers_filter = gtk_tree_model_filter_new (
-			GTK_TREE_MODEL (gebr.ui_server_list->common.store),
-			NULL);
-
-	gtk_tree_model_filter_set_visible_func (
-			GTK_TREE_MODEL_FILTER (gebr.ui_project_line->servers_filter),
-			servers_filter_visible_func, NULL, NULL);
-
-	gtk_combo_box_set_model (
-			GTK_COMBO_BOX (gebr.ui_flow_edition->server_combobox),
-			gebr.ui_project_line->servers_filter);
-
 	/* check for a menu list change */
 	if (menu_refresh_needed() == TRUE) {
 		menu_list_create_index();
@@ -416,6 +404,18 @@ gint gebr_config_load()
 
 	/* PROJECT/LINE/FLOWS */
 	project_list_populate();
+
+	gebr.ui_project_line->servers_filter = gtk_tree_model_filter_new (
+			GTK_TREE_MODEL (gebr.ui_server_list->common.store),
+			NULL);
+
+	gtk_tree_model_filter_set_visible_func (
+			GTK_TREE_MODEL_FILTER (gebr.ui_project_line->servers_filter),
+			servers_filter_visible_func, NULL, NULL);
+
+	gtk_combo_box_set_model (
+			GTK_COMBO_BOX (gebr.ui_flow_edition->server_combobox),
+			gebr.ui_project_line->servers_filter);
 
 	gboolean project_line_selected;
 	if (gebr_g_key_file_has_key(gebr.config.key_file, "general", "notebook")) {
