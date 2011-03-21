7b5356da1a57c3b553239881ce2cc6f9dee7f626
jorge.pzt@gebrproject.com <jorge.pzt@gebrproject.com>
2011-03-21 13:31:18 -0300

    Test for functions on libgebr/geoxml/project.h created.

---
+++ libgebr/geoxml/tests/test-project.c
@@ -0,0 +1,131 @@
+/*   libgebr - GêBR Library
+ *   Copyright (C) 2007-2009 GeBR core team (http://www.gebrproject.com/)
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <glib.h>
+
+#include "project.h"
+
+void test_gebr_geoxml_project_remove_line(void)
+{
+	GebrGeoXmlProject *project = NULL;
+	int check;
+
+	project = gebr_geoxml_project_new();
+	check = gebr_geoxml_project_has_line(project,"line1");
+	g_assert_cmpint(check,==,0);
+
+	gebr_geoxml_project_append_line(project,"line1");
+	check = gebr_geoxml_project_has_line(project,"line1");
+	g_assert_cmpint(check,==,1);
+	check = gebr_geoxml_project_has_line(project,"line2");
+	g_assert_cmpint(check,==,0);
+
+	gebr_geoxml_project_remove_line(project,"line1");
+	check = gebr_geoxml_project_has_line(project,"line1");
+	g_assert_cmpint(check,==,0);
+
+	check = gebr_geoxml_project_remove_line(project,"line1");
+	g_assert_cmpint(check,==,0);
+}
+
+void test_gebr_geoxml_project_get_lines_number(void)
+{
+	GebrGeoXmlProject *project = NULL;
+	glong n;
+
+	n = gebr_geoxml_project_get_lines_number(project);
+	g_assert_cmpint(n,==,-1);
+
+	project = gebr_geoxml_project_new();
+	n = gebr_geoxml_project_get_lines_number(project);
+	g_assert_cmpint(n,==,0);
+
+	gebr_geoxml_project_append_line(project,"line1");
+	n = gebr_geoxml_project_get_lines_number(project);
+	g_assert_cmpint(n,==,1);
+}
+
+void test_gebr_geoxml_project_get_line(void)
+{
+	GebrGeoXmlProject *project = NULL;
+	GebrGeoXmlSequence *sequence = NULL;
+	GebrGeoXmlProjectLine *project_line;
+	const gchar *source;
+	int check;
+
+	project = gebr_geoxml_project_new();
+
+	check = gebr_geoxml_project_get_line(project, &sequence, 0);
+	g_assert_cmpint(check,==,-5);
+
+	project_line = gebr_geoxml_project_append_line(project,"line1");
+	check = gebr_geoxml_project_get_line(project, &sequence, 0);
+	g_assert_cmpint(check,==,0);
+	//compare if project_line created is equal a sequence passed by function get_line
+	g_assert(project_line == GEBR_GEOXML_PROJECT_LINE(sequence));
+	//now, verified if the name of project_line returned is the same of the get_line created
+	source = gebr_geoxml_project_get_line_source(GEBR_GEOXML_PROJECT_LINE(sequence));
+	g_assert_cmpstr(source,==,"line1");
+}
+
+void test_gebr_geoxml_project_set_line_source(void)
+{
+	GebrGeoXmlProject *project = NULL;
+	GebrGeoXmlProjectLine *project_line;
+	const gchar *source;
+	int check;
+
+	project = gebr_geoxml_project_new();
+
+	project_line = gebr_geoxml_project_append_line(project,"line1");
+	source = gebr_geoxml_project_get_line_source(project_line);
+	g_assert_cmpstr(source,==,"line1");
+
+	gebr_geoxml_project_set_line_source(project_line,"line-diff");
+	source = gebr_geoxml_project_get_line_source(project_line);
+	g_assert_cmpstr(source,==,"line-diff");
+}
+
+void test_gebr_geoxml_project_get_line_from_source(void)
+{
+	GebrGeoXmlProject *project = NULL;
+	GebrGeoXmlProjectLine *project_line, *receive;
+	const gchar *source;
+
+	project = gebr_geoxml_project_new();
+	project_line = gebr_geoxml_project_append_line(project,"line1");
+
+	receive = gebr_geoxml_project_get_line_from_source(project,"line2");
+	g_assert(receive == NULL);
+
+	receive = gebr_geoxml_project_get_line_from_source(project,"line1");
+	g_assert(receive == project_line);
+	source = gebr_geoxml_project_get_line_source(receive);
+	g_assert_cmpstr(source,==,"line1");
+}
+
+int main(int argc, char *argv[])
+{
+	g_test_init(&argc, &argv, NULL);
+
+	g_test_add_func("/libgebr/geoxml/project/remove_line",test_gebr_geoxml_project_remove_line);
+	g_test_add_func("/libgebr/geoxml/project/get_number_line",test_gebr_geoxml_project_get_lines_number);
+	g_test_add_func("/libgebr/geoxml/project/set_line_source",test_gebr_geoxml_project_set_line_source);
+	g_test_add_func("/libgebr/geoxml/project/get_line_from_source",test_gebr_geoxml_project_get_line_from_source);
+
+	return g_test_run();
+}
