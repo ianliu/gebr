6fd7c0b4a0f0b6445dd10942cccac81985181f1a
sobral@gebrproject.com <sobral@gebrproject.com>
2011-03-30 15:38:43 -0300

    Create more test functions for /libgebr/geoxml/object.

--- libgebr/geoxml/tests/test-object.c
+++ libgebr/geoxml/tests/test-object.c
@@ -18,48 +18,145 @@
 #include <glib.h>
 #include <stdlib.h>
 
+#include "program.h"
 #include "object.h"
 #include "document.h"
 #include "flow.h"
 #include "line.h"
 #include "project.h"
+#include "program-parameter.h"
 
 void test_gebr_geoxml_object_get_type (void)
 {
-	GebrGeoXmlObject *object = NULL;
+	GebrGeoXmlObject *object;
+	GebrGeoXmlObject *flow;
+	GebrGeoXmlObject *program;
+	GebrGeoXmlObject *parameters;
+	GebrGeoXmlObject *parameter;
 	GebrGeoXmlObjectType type = GEBR_GEOXML_OBJECT_TYPE_UNKNOWN;
-//	GebrGeoXmlDocument *document;
-//	GebrGeoXmlProgram *program;
 
 	//Test if trying to get the type of a NULL object will fail, as expected
 	if (g_test_trap_fork (0, G_TEST_TRAP_SILENCE_STDOUT | G_TEST_TRAP_SILENCE_STDERR)) {
-		type = gebr_geoxml_object_get_type(object);
+		type = gebr_geoxml_object_get_type(NULL);
 		exit(0);
 	}
 	g_test_trap_assert_failed();
 
-	object = GEBR_GEOXML_OBJECT (gebr_geoxml_flow_new());
+	object = GEBR_GEOXML_OBJECT(gebr_geoxml_project_new());
 	type = gebr_geoxml_object_get_type(object);
-	g_assert(type == GEBR_GEOXML_OBJECT_TYPE_FLOW);
+	g_assert_cmpint(type, ==, GEBR_GEOXML_OBJECT_TYPE_PROJECT);
 
-	object = GEBR_GEOXML_OBJECT (gebr_geoxml_project_new());
+	object = GEBR_GEOXML_OBJECT(gebr_geoxml_line_new());
 	type = gebr_geoxml_object_get_type(object);
-	g_assert(type == GEBR_GEOXML_OBJECT_TYPE_PROJECT);
+	g_assert_cmpint(type, ==, GEBR_GEOXML_OBJECT_TYPE_LINE);
 
-	object = GEBR_GEOXML_OBJECT (gebr_geoxml_line_new());
-	type = gebr_geoxml_object_get_type(object);
-	g_assert(type == GEBR_GEOXML_OBJECT_TYPE_LINE);
+	flow = GEBR_GEOXML_OBJECT(gebr_geoxml_flow_new());
+	type = gebr_geoxml_object_get_type(flow);
+	g_assert_cmpint(type, ==, GEBR_GEOXML_OBJECT_TYPE_FLOW);
 
-//	gebr_geoxml_document_load(&document, TEST_DIR"/test.mnu", FALSE, NULL);
-//
-//	gebr_geoxml_flow_get_program(document, &program, 0);
-//	type = gebr_geoxml_object_get_type(program);
-//	g_assert(type == GEBR_GEOXML_OBJECT_TYPE_PROGRAM);
-//
-//	object = gebr_geoxml_program_get_parameters(program);
-//	type = gebr_geoxml_object_get_type(object);
-//	g_assert(type == GEBR_GEOXML_OBJECT_TYPE_PARAMETERS);
+	program = GEBR_GEOXML_OBJECT(gebr_geoxml_flow_append_program(GEBR_GEOXML_FLOW(flow)));
+	type = gebr_geoxml_object_get_type(program);
+	g_assert_cmpint(type, ==, GEBR_GEOXML_OBJECT_TYPE_PROGRAM);
 
+	parameters = GEBR_GEOXML_OBJECT(gebr_geoxml_program_get_parameters(GEBR_GEOXML_PROGRAM(program)));
+	type = gebr_geoxml_object_get_type(parameters);
+	g_assert_cmpint(type, ==, GEBR_GEOXML_OBJECT_TYPE_PARAMETERS);
+
+	parameter = GEBR_GEOXML_OBJECT(gebr_geoxml_parameters_append_parameter(GEBR_GEOXML_PARAMETERS(parameters), 0));
+	type = gebr_geoxml_object_get_type(parameter);
+	g_assert_cmpint(type, ==, GEBR_GEOXML_OBJECT_TYPE_PARAMETER);
+}
+
+void test_gebr_geoxml_object_get_user_data (void)
+{
+	GebrGeoXmlObject * object;
+	GebrGeoXmlObject * program;
+	gpointer data = "foo";
+	gpointer user_data;
+
+	//Test if \p object is NULL will fail, as expected
+	if (g_test_trap_fork (0, G_TEST_TRAP_SILENCE_STDOUT | G_TEST_TRAP_SILENCE_STDERR)) {
+		gebr_geoxml_object_set_user_data(NULL, data);
+		exit(0);
+	}
+	g_test_trap_assert_failed();
+
+	if (g_test_trap_fork (0, G_TEST_TRAP_SILENCE_STDOUT | G_TEST_TRAP_SILENCE_STDERR)) {
+		user_data = gebr_geoxml_object_get_user_data(NULL);
+		exit(0);
+	}
+	g_test_trap_assert_failed();
+
+	object = GEBR_GEOXML_OBJECT(gebr_geoxml_flow_new());
+	gebr_geoxml_object_set_user_data(object, data);
+	user_data = gebr_geoxml_object_get_user_data(object);
+	g_assert_cmpstr((char *)user_data, ==, "foo");
+
+	program = GEBR_GEOXML_OBJECT(gebr_geoxml_flow_append_program(GEBR_GEOXML_FLOW(object)));
+	gebr_geoxml_object_set_user_data(program, data);
+	user_data = gebr_geoxml_object_get_user_data(program);
+	g_assert_cmpstr((char *)user_data, ==, "foo");
+}
+
+void test_gebr_geoxml_object_generate_help (void)
+{
+	GebrGeoXmlObject *object;
+	GebrGeoXmlProgram *program;
+	const gchar *help;
+
+	//Test if \p object is NULL will fail, as expected
+	if (g_test_trap_fork (0, G_TEST_TRAP_SILENCE_STDOUT | G_TEST_TRAP_SILENCE_STDERR)) {
+		help = gebr_geoxml_object_generate_help(NULL, "foo");
+		exit(0);
+	}
+	g_test_trap_assert_failed();
+
+	object = GEBR_GEOXML_OBJECT(gebr_geoxml_flow_new());
+	program = gebr_geoxml_flow_append_program(GEBR_GEOXML_FLOW(object));
+	if (g_test_trap_fork (0, G_TEST_TRAP_SILENCE_STDOUT | G_TEST_TRAP_SILENCE_STDERR)) {
+		gebr_geoxml_program_set_title(program, "Test Program");
+		gebr_geoxml_program_set_description(program, "This is a test program.");
+		gebr_geoxml_program_set_version(program, "1.0");
+		help = gebr_geoxml_object_generate_help(GEBR_GEOXML_OBJECT(program), "Help content.");
+		g_print("%s", help);
+		exit(0);
+	}
+	g_test_trap_assert_passed();
+
+	g_test_trap_assert_stdout ("*begin ttl*Test Program*end ttl*");
+	g_test_trap_assert_stdout ("*begin tt2*Test Program*end tt2*");
+	g_test_trap_assert_stdout ("*begin des*This is a test program.*end des*");
+	g_test_trap_assert_stdout ("*begin cnt*Help content.*end cnt*");
+	g_test_trap_assert_stdout ("*begin ver*1.0*end ver*");
+}
+
+void test_gebr_geoxml_object_get_help_content(void)
+{
+	GebrGeoXmlObject *object;
+	GebrGeoXmlProgram *program;
+	const gchar *help_cnt;
+	const gchar *help;
+
+	//Test if \p object is NULL will fail, as expected
+	if (g_test_trap_fork (0, G_TEST_TRAP_SILENCE_STDOUT | G_TEST_TRAP_SILENCE_STDERR)) {
+		help_cnt = gebr_geoxml_object_get_help_content(NULL);
+		exit(0);
+	}
+	g_test_trap_assert_failed();
+
+	//Test if \p object isn't a flow or a program will fail, as expected
+	if (g_test_trap_fork (0, G_TEST_TRAP_SILENCE_STDOUT | G_TEST_TRAP_SILENCE_STDERR)) {
+		help_cnt = gebr_geoxml_object_get_help_content(GEBR_GEOXML_OBJECT(gebr_geoxml_line_new()));
+		exit(0);
+	}
+	g_test_trap_assert_failed();
+
+	object = GEBR_GEOXML_OBJECT(gebr_geoxml_flow_new());
+	program = gebr_geoxml_flow_append_program(GEBR_GEOXML_FLOW(object));
+	help = gebr_geoxml_object_generate_help(GEBR_GEOXML_OBJECT(program), "Help content.");
+	gebr_geoxml_program_set_help(program, help);
+	help_cnt = gebr_geoxml_object_get_help_content(GEBR_GEOXML_OBJECT(program));
+	g_assert_cmpstr(help_cnt, ==, "Help content.");
 }
 
 int main(int argc, char *argv[])
@@ -67,6 +164,9 @@ int main(int argc, char *argv[])
 	g_test_init(&argc, &argv, NULL);
 
 	g_test_add_func("/libgebr/geoxml/object/get_type", test_gebr_geoxml_object_get_type);
+	g_test_add_func("/libgebr/geoxml/object/get_and_set_user_data", test_gebr_geoxml_object_get_user_data);
+	g_test_add_func("/libgebr/geoxml/object/generate_help", test_gebr_geoxml_object_generate_help);
+	g_test_add_func("/libgebr/geoxml/object/get_help_content", test_gebr_geoxml_object_get_help_content);
 
 	return g_test_run();
 }
