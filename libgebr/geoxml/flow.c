399a6b8fe92761014e30c355b1ae0f0add545df8
Ian Liu Rodrigues <ian.liu88@gmail.com>
2011-04-05 11:25:29 -0300

    Insert `iter' on top of flow's dictionary

--- libgebr/geoxml/flow.c
+++ libgebr/geoxml/flow.c
@@ -685,9 +685,6 @@ static gboolean gebr_geoxml_flow_insert_iter_dict (GebrGeoXmlFlow *flow)
 	GebrGeoXmlParameters *dict;
 	const gchar *keyword;
 
-	if (!gebr_geoxml_flow_has_control_program (flow))
-		return FALSE;
-
 	dict = gebr_geoxml_document_get_dict_parameters (GEBR_GEOXML_DOCUMENT (flow));
 	seq = gebr_geoxml_parameters_get_first_parameter (dict);
 	keyword = gebr_geoxml_program_parameter_get_keyword (GEBR_GEOXML_PROGRAM_PARAMETER (seq));
@@ -698,6 +695,7 @@ static gboolean gebr_geoxml_flow_insert_iter_dict (GebrGeoXmlFlow *flow)
 	param = gebr_geoxml_parameters_append_parameter (dict, GEBR_GEOXML_PARAMETER_TYPE_INT);
 	gebr_geoxml_program_parameter_set_keyword (GEBR_GEOXML_PROGRAM_PARAMETER (param), "iter");
 	gebr_geoxml_parameter_set_label (param, _("Loop iterator"));
+	gebr_geoxml_sequence_move_after (GEBR_GEOXML_SEQUENCE (param), NULL);
 
 	return TRUE;
 }
