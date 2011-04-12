0ae616a9922a3deb086ec564f800cd39761e1585
fmatheus <none@none>
2011-04-12 17:36:29 -0300

    Update changelogs before release.
    --HG--
    branch : 0.12

--- hg2cl.sh
+++ hg2cl.sh
@@ -1,9 +1,12 @@
-hg --quiet log -M --style=changelog $* | perl -pe '
-s/  brauliobo.*>$/  Braulio Barros de Oliveira  <brauliobo\@gebrproject.com>/;
+hg --quiet log --style=changelog $* | perl -pe '
+s/  braulio.*>$/  Braulio Barros de Oliveira  <brauliobo\@gebrproject.com>/i;
 s/  (ian|ianliu).*>$/  Ian Liu Rodrigues  <ian.liu\@gebrproject.com>/i;
 s/  biloti.*>$/  Ricardo Biloti  <biloti\@gebrproject.com>/;
 s/  fmatheus.*>$/  Fabrício Matheus Gonçalvez  <fmatheus\@gebrproject.com>/;
-s/  abaaklini.*>$/  Alexandre Baaklini Gomes Coelho  <abaaklini\@gebrproject.com>/;
+s/  (alexandre|abaaklini).*>$/  Alexandre Baaklini Gomes Coelho  <abaaklini\@gebrproject.com>/i;
 s/  rodrigo.*>$/  Rodrigo Morelatto  <morelatto\@gebrproject.com>/i;
+s/  jorge.*>$/  Jorge Pizzolatto <jorge.pzt\@gebrproject.com>/i;
+s/  sobral.*>/  Gabriel Sobral <sobral\@gebrproject.com>/i;
+s/  davi.*>$/  Davi Clemente <davi.clemente\@gebrproject.com>/i;
 s/  (fabioaz|fabiosazevedo).*>$/  Fabio de Souza Azevedo  <fabioaz\@gebrproject.com>/' | tail -n +2
 echo
