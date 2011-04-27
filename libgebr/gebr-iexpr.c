af6011f7e03df7838aabe90fd95914eff39c5557
Ian Liu Rodrigues <ian.liu@gebrproject.com>
2011-04-27 00:58:37 -0300

    Add GebrIExpr interface

---
+++ libgebr/gebr-iexpr.c
@@ -0,0 +1,39 @@
+/*   libgebr - GeBR Library
+ *   Copyright (C) 2011 GeBR core team (http://www.gebrproject.com/)
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "gebr-iexpr.h"
+
+G_DEFINE_INTERFACE(GebrIExpr, gebr_iexpr, G_TYPE_INTERFACE);
+
+static void gebr_iexpr_default_init(GebrIExprInterface *iface)
+{
+}
+
+gboolean gebr_iexpr_set_var(GebrIExpr              *self,
+			    const gchar            *name,
+			    GebrGeoXmlParameterType type,
+			    GError                **error)
+{
+	return GEBR_IEXPR_GET_INTERFACE(self)->set_var(self, name, type, error);
+}
+
+gboolean gebr_iexpr_is_valid(GebrIExpr   *self,
+			     const gchar *expr,
+			     GError     **error)
+{
+	return GEBR_IEXPR_GET_INTERFACE(self)->is_valid(self, expr, error);
+}
---
+++ libgebr/gebr-iexpr.h
@@ -0,0 +1,107 @@
+/*   libgebr - GeBR Library
+ *   Copyright (C) 2011 GeBR core team (http://www.gebrproject.com/)
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * SECTION: gebr-iexpr
+ * @title: GebrIExpr Interface
+ * @short_description: Interface for defining expressions and validate them.
+ *
+ * The #GebrIExpr interface defines methods for creating and validating expressions.
+ * There are two kinds of expressions in GeBR: #GebrArithmeticExpr and #GebrStringExpr.
+ */
+
+#ifndef __LIBGEBR_IEXPR_H__
+#define __LIBGEBR_IEXPR_H__
+
+#include <glib.h>
+#include <glib-object.h>
+#include <geoxml/parameter.h>
+
+G_BEGIN_DECLS
+
+#define GEBR_IEXPR_TYPE			(gebr_iexpr_get_type())
+#define GEBR_IEXPR(obj)			(G_TYPE_CHECK_INSTANCE_CAST((obj), GEBR_IEXPR_TYPE, GebrIExpr))
+#define GEBR_IS_IEXPR(obj)		(G_TYPE_CHECK_INSTANCE_TYPE((obj), GEBR_IEXPR_TYPE))
+#define GEBR_IEXPR_GET_INTERFACE(inst)	(G_TYPE_INSTANCE_GET_INTERFACE((inst), GEBR_IEXPR_TYPE, GebrIExprInterface))
+
+/**
+ * GEBR_IEXPR_ERROR:
+ *
+ * Error domain for handling expressions. Errors in this domain
+ * will be from #GebrIExprError enumeration.
+ */
+#define GEBR_IEXPR_ERROR (gebr_iexpr_error_quark())
+
+/**
+ * GebrIExprError:
+ * @GEBR_IEXPR_ERROR_INVAL_VAR: An invalid variable name was found.
+ * @GEBR_IEXPR_ERROR_INVAL_EXPR: The expression is invalid.
+ *
+ * Error codes returned by expression handling functions.
+ */
+typedef enum {
+	GEBR_IEXPR_ERROR_INVAL_VAR,
+	GEBR_IEXPR_ERROR_INVAL_EXPR,
+} GebrIExprError;
+
+typedef struct _GebrIExpr GebrIExpr;
+typedef struct _GebrIExprInterface GebrIExprInterface;
+
+struct _GebrIExprInterface {
+	GTypeInterface parent;
+
+	gboolean (*set_var) (GebrIExpr              *self,
+			     const gchar            *name,
+			     GebrGeoXmlParameterType type,
+			     GError                **error);
+
+	gboolean (*is_valid) (GebrIExpr   *self,
+			      const gchar *expr,
+			      GError     **error);
+};
+
+GType gebr_iexpr_get_type(void) G_GNUC_CONST;
+
+/**
+ * gebr_iexpr_set_var:
+ * @expr: a #GebrIExpr
+ * @name: the name of the variable
+ * @type: one of #GebrGeoXmlParameterType
+ * @error: return location for an #GEBR_IEXPR_ERROR, or %NULL
+ *
+ * Returns: %TRUE if variable was successfully set, %FALSE otherwise.
+ */
+gboolean gebr_iexpr_set_var(GebrIExpr              *self,
+			    const gchar            *name,
+			    GebrGeoXmlParameterType type,
+			    GError                **error);
+
+/**
+ * gebr_iexpr_is_valid:
+ * @expr: a #GebrIExpr
+ * @expression: the expression to check for validity
+ * @error: return location for an #GEBR_IEXPR_ERROR, or %NULL
+ *
+ * Returns: %TRUE if @expression is valid, %FALSE otherwise.
+ */
+gboolean gebr_iexpr_is_valid(GebrIExpr   *self,
+			     const gchar *expr,
+			     GError     **error);
+
+G_END_DECLS
+
+#endif /* __LIBGEBR_IEXPR_H__ */
