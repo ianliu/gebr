f70ab9bf00deede58452e70698b8379b0561cbfe
davi.clemente@gebrproject.com <davi.clemente@gebrproject.com>
2011-04-29 15:06:09 -0300

    New unit tests.

--- libgebr/geoxml/tests/test-parameters.c
+++ libgebr/geoxml/tests/test-parameters.c
@@ -58,11 +58,11 @@ void test_gebr_geoxml_parameters_is_var_used()
 
 	parameter = gebr_geoxml_parameters_append_parameter(parameters_list, GEBR_GEOXML_PARAMETER_TYPE_INT);
 	gebr_geoxml_program_parameter_set_keyword(GEBR_GEOXML_PROGRAM_PARAMETER(parameter), "Test keyword 1");
-	gebr_geoxml_program_parameter_set_string_value(GEBR_GEOXML_PROGRAM_PARAMETER(parameter),FALSE,"a+var1");
+	gebr_geoxml_program_parameter_set_string_value(GEBR_GEOXML_PROGRAM_PARAMETER(parameter),FALSE, "a+var1");
 
 	parameter = gebr_geoxml_parameters_append_parameter(parameters_list, GEBR_GEOXML_PARAMETER_TYPE_INT);
 	gebr_geoxml_program_parameter_set_keyword(GEBR_GEOXML_PROGRAM_PARAMETER(parameter), "Test keyword 2");
-	gebr_geoxml_program_parameter_set_string_value(GEBR_GEOXML_PROGRAM_PARAMETER(parameter),FALSE,"a+var2");
+	gebr_geoxml_program_parameter_set_string_value(GEBR_GEOXML_PROGRAM_PARAMETER(parameter),FALSE, "a+var2");
 
 	g_assert(gebr_geoxml_parameters_is_var_used(parameters_list, "var2") == TRUE);
 	g_assert(gebr_geoxml_program_parameter_is_var_used(GEBR_GEOXML_PROGRAM_PARAMETER(parameter), "var2") == TRUE);
@@ -75,12 +75,8 @@ void test_gebr_geoxml_parameters_get_and_set_default_selection(void)
 	GebrGeoXmlFlow *flow;
 	GebrGeoXmlProgram *program;
 
-	// Test if passing NULL to get_label will fail
-	if (g_test_trap_fork (0, G_TEST_TRAP_SILENCE_STDOUT | G_TEST_TRAP_SILENCE_STDERR)) {
-		gebr_geoxml_parameter_get_label(NULL);
-		exit(0);
-	}
-	g_test_trap_assert_failed();
+	// Check if will return NULL when program is passed as NULL
+	g_assert(!gebr_geoxml_parameters_get_default_selection(NULL));
 
 	flow = gebr_geoxml_flow_new();
 	program = gebr_geoxml_flow_append_program(flow);
@@ -94,6 +90,112 @@ void test_gebr_geoxml_parameters_get_and_set_default_selection(void)
 	g_assert_cmpstr(gebr_geoxml_parameter_get_label(parameter), ==, "Default parameter");
 }
 
+void test_gebr_geoxml_parameters_get_and_set_selection(void)
+{
+	GebrGeoXmlParameters *parameters_list;
+	GebrGeoXmlParameter *parameter;
+	GebrGeoXmlFlow *flow;
+	GebrGeoXmlProgram *program;
+
+	// Check if will return NULL when program is passed as NULL
+	g_assert(!gebr_geoxml_parameters_get_selection(NULL));
+
+	flow = gebr_geoxml_flow_new();
+	program = gebr_geoxml_flow_append_program(flow);
+	parameters_list = gebr_geoxml_program_get_parameters(program);
+	parameter = gebr_geoxml_parameters_append_parameter(parameters_list, GEBR_GEOXML_PARAMETER_TYPE_STRING);
+	gebr_geoxml_parameter_set_label(parameter, "A parameter");
+	// Need to be set as default selection before
+	gebr_geoxml_parameters_set_default_selection(parameters_list, parameter);
+
+	gebr_geoxml_parameters_set_selection(parameters_list, parameter);
+	parameter = gebr_geoxml_parameters_get_selection(parameters_list);
+
+	g_assert_cmpstr(gebr_geoxml_parameter_get_label(parameter), ==, "A parameter");
+}
+
+void test_gebr_geoxml_parameters_get_first_parameter(void)
+{
+	GebrGeoXmlParameters *parameters_list;
+	GebrGeoXmlParameter *parameter;
+	GebrGeoXmlFlow *flow;
+	GebrGeoXmlProgram *program;
+
+	flow = gebr_geoxml_flow_new();
+	program = gebr_geoxml_flow_append_program(flow);
+	parameters_list = gebr_geoxml_program_get_parameters(program);
+
+	parameter = gebr_geoxml_parameters_append_parameter(parameters_list, GEBR_GEOXML_PARAMETER_TYPE_STRING);
+	gebr_geoxml_parameter_set_label(parameter, "1st parameter");
+
+	parameter = gebr_geoxml_parameters_append_parameter(parameters_list, GEBR_GEOXML_PARAMETER_TYPE_STRING);
+	gebr_geoxml_parameter_set_label(parameter, "2nd parameter");
+
+	parameter = (GebrGeoXmlParameter*) gebr_geoxml_parameters_get_first_parameter(parameters_list);
+	g_assert_cmpstr(gebr_geoxml_parameter_get_label(parameter), ==, "1st parameter");
+}
+
+void test_gebr_geoxml_parameters_get_parameter(void)
+{
+	GebrGeoXmlParameters *parameters_list;
+	GebrGeoXmlParameter *parameter;
+	GebrGeoXmlFlow *flow;
+	GebrGeoXmlProgram *program;
+	int index;
+
+	flow = gebr_geoxml_flow_new();
+	program = gebr_geoxml_flow_append_program(flow);
+	parameters_list = gebr_geoxml_program_get_parameters(program);
+
+	parameter = gebr_geoxml_parameters_append_parameter(parameters_list, GEBR_GEOXML_PARAMETER_TYPE_STRING);
+	gebr_geoxml_parameter_set_label(parameter, "1st parameter");
+
+	parameter = gebr_geoxml_parameters_append_parameter(parameters_list, GEBR_GEOXML_PARAMETER_TYPE_STRING);
+	gebr_geoxml_parameter_set_label(parameter, "2nd parameter");
+
+	index = gebr_geoxml_parameters_get_parameter(parameters_list, (GebrGeoXmlSequence**) &parameter, 0);
+	g_assert_cmpint(index, ==, GEBR_GEOXML_RETV_SUCCESS);
+	g_assert_cmpstr(gebr_geoxml_parameter_get_label(parameter), ==, "1st parameter");
+
+	index = gebr_geoxml_parameters_get_parameter(parameters_list, (GebrGeoXmlSequence**) &parameter, 1);
+	g_assert_cmpint(index, ==, GEBR_GEOXML_RETV_SUCCESS);
+	g_assert_cmpstr(gebr_geoxml_parameter_get_label(parameter), ==, "2nd parameter");
+
+	index = gebr_geoxml_parameters_get_parameter(parameters_list, (GebrGeoXmlSequence**) &parameter, 1337);
+	g_assert_cmpint(index, ==, GEBR_GEOXML_RETV_INVALID_INDEX);
+
+	index = gebr_geoxml_parameters_get_parameter(NULL, (GebrGeoXmlSequence**) &parameter, 0);
+	g_assert_cmpint(index, ==, GEBR_GEOXML_RETV_NULL_PTR);
+}
+
+void test_gebr_geoxml_parameters_get_number(void)
+{
+	GebrGeoXmlParameters *parameters_list;
+	GebrGeoXmlParameter *parameter;
+	GebrGeoXmlFlow *flow;
+	GebrGeoXmlProgram *program;
+
+	flow = gebr_geoxml_flow_new();
+	program = gebr_geoxml_flow_append_program(flow);
+	parameters_list = gebr_geoxml_program_get_parameters(program);
+
+	g_assert_cmpint(gebr_geoxml_parameters_get_number(NULL), ==, -1);
+
+	g_assert_cmpint(gebr_geoxml_parameters_get_number(parameters_list), ==, 0);
+
+	parameter = gebr_geoxml_parameters_append_parameter(parameters_list, GEBR_GEOXML_PARAMETER_TYPE_STRING);
+	gebr_geoxml_parameter_set_label(parameter, "1st parameter");
+
+	g_assert_cmpint(gebr_geoxml_parameters_get_number(parameters_list), ==, 1);
+
+	parameter = gebr_geoxml_parameters_append_parameter(parameters_list, GEBR_GEOXML_PARAMETER_TYPE_STRING);
+	gebr_geoxml_parameter_set_label(parameter, "2nd parameter");
+	parameter = gebr_geoxml_parameters_append_parameter(parameters_list, GEBR_GEOXML_PARAMETER_TYPE_STRING);
+	gebr_geoxml_parameter_set_label(parameter, "3rd parameter");
+
+	g_assert_cmpint(gebr_geoxml_parameters_get_number(parameters_list), ==, 3);
+}
+
 int main(int argc, char *argv[])
 {
 	g_test_init(&argc, &argv, NULL);
@@ -101,6 +203,10 @@ int main(int argc, char *argv[])
 	g_test_add_func("/libgebr/geoxml/parameters/append_parameter", test_gebr_geoxml_parameters_append_parameter);
 	g_test_add_func("/libgebr/geoxml/parameters/is_var_used", test_gebr_geoxml_parameters_is_var_used);
 	g_test_add_func("/libgebr/geoxml/parameters/get_and_set_default_selection", test_gebr_geoxml_parameters_get_and_set_default_selection);
+	g_test_add_func("/libgebr/geoxml/parameters/get_and_set_selection", test_gebr_geoxml_parameters_get_and_set_selection);
+	g_test_add_func("/libgebr/geoxml/parameters/get_first_parameter", test_gebr_geoxml_parameters_get_first_parameter);
+	g_test_add_func("/libgebr/geoxml/parameters/get_parameter", test_gebr_geoxml_parameters_get_parameter);
+	g_test_add_func("/libgebr/geoxml/parameters/get_number", test_gebr_geoxml_parameters_get_number);
 
 	return g_test_run();
 }
