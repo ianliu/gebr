aa4d3feb8dfa4f0749764344e9b889ec6b50cdf6
jorge.pzt@gebrproject.com <jorge.pzt@gebrproject.com>
2011-08-03 18:31:59 -0300

    Fixes freeze of gebr when try to connect on another server.

--- gebr/server.c
+++ gebr/server.c
@@ -198,7 +198,7 @@ static void server_state_changed(struct gebr_comm_server *comm_server, GebrServe
  */
 static GString *server_ssh_login(const gchar * title, const gchar * message)
 {
-
+	gdk_threads_enter();
 	GtkWidget *dialog = gtk_dialog_new_with_buttons(title, GTK_WINDOW(gebr.window),
 							(GtkDialogFlags)(GTK_DIALOG_MODAL |
 									 GTK_DIALOG_DESTROY_WITH_PARENT),
@@ -219,7 +219,7 @@ static GString *server_ssh_login(const gchar * title, const gchar * message)
 	GString *password = !confirmed ? NULL : g_string_new(gtk_entry_get_text(GTK_ENTRY(entry)));
 
 	gtk_widget_destroy(dialog);
-
+	gdk_threads_leave();
 	return password;
 }
 
@@ -228,7 +228,10 @@ static GString *server_ssh_login(const gchar * title, const gchar * message)
  */
 static gboolean server_ssh_question(const gchar * title, const gchar * message)
 {
-	return gebr_gui_confirm_action_dialog(title, message);
+	gdk_threads_enter();
+	gboolean response = gebr_gui_confirm_action_dialog(title, message);
+	gdk_threads_leave();
+	return response;
 }
 
 /**
