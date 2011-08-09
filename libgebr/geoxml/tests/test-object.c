0288b22b2fb8876261fe32d8749133d9890400c4
giuliano@gebrproject.com <giuliano@gebrproject.com>
2011-08-09 16:31:24 -0300

    Freeing memory in GeoXML Object tests.
    --HG--
    branch : memory-leak-fix

--- libgebr/geoxml/tests/test-object.c
+++ libgebr/geoxml/tests/test-object.c
@@ -47,9 +47,13 @@ void test_gebr_geoxml_object_get_type (void)
 	type = gebr_geoxml_object_get_type(object);
 	g_assert_cmpint(type, ==, GEBR_GEOXML_OBJECT_TYPE_PROJECT);
 
+	gebr_geoxml_document_free(GEBR_GEOXML_DOC(object));
+
 	object = GEBR_GEOXML_OBJECT(gebr_geoxml_line_new());
 	type = gebr_geoxml_object_get_type(object);
 	g_assert_cmpint(type, ==, GEBR_GEOXML_OBJECT_TYPE_LINE);
+
+	gebr_geoxml_document_free(GEBR_GEOXML_DOC(object));
 
 	flow = GEBR_GEOXML_OBJECT(gebr_geoxml_flow_new());
 	type = gebr_geoxml_object_get_type(flow);
@@ -66,6 +70,11 @@ void test_gebr_geoxml_object_get_type (void)
 	parameter = GEBR_GEOXML_OBJECT(gebr_geoxml_parameters_append_parameter(GEBR_GEOXML_PARAMETERS(parameters), 0));
 	type = gebr_geoxml_object_get_type(parameter);
 	g_assert_cmpint(type, ==, GEBR_GEOXML_OBJECT_TYPE_PARAMETER);
+
+	gebr_geoxml_object_unref(parameter);
+	gebr_geoxml_object_unref(parameters);
+	gebr_geoxml_object_unref(program);
+	gebr_geoxml_document_free(GEBR_GEOXML_DOC(flow));
 }
 
 void test_gebr_geoxml_object_get_user_data (void)
@@ -97,13 +106,16 @@ void test_gebr_geoxml_object_get_user_data (void)
 	gebr_geoxml_object_set_user_data(program, data);
 	user_data = gebr_geoxml_object_get_user_data(program);
 	g_assert_cmpstr((char *)user_data, ==, "foo");
+
+	gebr_geoxml_object_unref(program);
+	gebr_geoxml_document_free(GEBR_GEOXML_DOC(object));
 }
 
 void test_gebr_geoxml_object_generate_help (void)
 {
 	GebrGeoXmlObject *object;
 	GebrGeoXmlProgram *program;
-	const gchar *help;
+	gchar *help = NULL;
 
 	//Test if \p object is NULL will fail, as expected
 	if (g_test_trap_fork (0, G_TEST_TRAP_SILENCE_STDOUT | G_TEST_TRAP_SILENCE_STDERR)) {
@@ -129,14 +141,18 @@ void test_gebr_geoxml_object_generate_help (void)
 	g_test_trap_assert_stdout ("*begin des*This is a test program.*end des*");
 	g_test_trap_assert_stdout ("*begin cnt*Help content.*end cnt*");
 	g_test_trap_assert_stdout ("*begin ver*1.0*end ver*");
+
+	g_free(help);
+	gebr_geoxml_object_unref(program);
+	gebr_geoxml_document_free(GEBR_GEOXML_DOC(object));
 }
 
 void test_gebr_geoxml_object_get_help_content(void)
 {
 	GebrGeoXmlObject *object;
 	GebrGeoXmlProgram *program;
-	const gchar *help_cnt;
-	const gchar *help;
+	gchar *help_cnt = NULL;
+	gchar *help = NULL;
 
 	//Test if \p object is NULL will fail, as expected
 	if (g_test_trap_fork (0, G_TEST_TRAP_SILENCE_STDOUT | G_TEST_TRAP_SILENCE_STDERR)) {
@@ -158,6 +174,11 @@ void test_gebr_geoxml_object_get_help_content(void)
 	gebr_geoxml_program_set_help(program, help);
 	help_cnt = gebr_geoxml_object_get_help_content(GEBR_GEOXML_OBJECT(program));
 	g_assert_cmpstr(help_cnt, ==, "Help content.");
+
+	g_free(help_cnt);
+	g_free(help);
+	gebr_geoxml_object_unref(program);
+	gebr_geoxml_document_free(GEBR_GEOXML_DOC(object));
 }
 
 int main(int argc, char *argv[])
