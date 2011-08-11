5400265cc426ffcbb33f20a5a8372df4fb5c7a9a
Jorge Pizzolatto Filho <jorge.pzt@gebrproject.com>
2011-08-11 11:34:58 -0300

    Fix read after free in gebr/document.c
    --HG--
    branch : memory-leak-fix

--- gebr/document.c
+++ gebr/document.c
@@ -527,13 +527,14 @@ void document_import(GebrGeoXmlDocument * document, gboolean save)
 	}
 	GString *new_filename = document_assembly_filename(extension);
 	gebr_geoxml_document_set_filename(document, new_filename->str);
-	g_string_free(new_filename, TRUE);
 
 	if (save) {
 		GString *path = document_get_path(new_filename->str);
 		document_save_at(document, path->str, FALSE, TRUE);
 		g_string_free(path, TRUE);
 	}
+
+	g_string_free(new_filename, TRUE);
 }
 
 GString *document_assembly_filename(const gchar * extension)
