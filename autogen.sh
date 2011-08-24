6a63763a658542dcab0982beaeba6c0cd0c1d0c9
Ian Liu Rodrigues <ian.liu@gebrproject.com>
2011-08-24 16:15:24 -0300

    Include patched gtk-doc.make into gebrproject and remove gtkdocize call

--- autogen.sh
+++ autogen.sh
@@ -1,3 +1,7 @@
 libtoolize --copy --force
-gtkdocize
+# Don't run gtkdocize since it will overwrite the gtk-doc.make
+# which has some changes to solve a 'distcheck' problem.
+# The only this it does is copy the gtk-doc.make and gtk-doc.m4,
+# which are already in the package.
+# gtkdocize
 autoreconf -i
