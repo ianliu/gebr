be67a7c8b1b17bb5e6758dbf7d1949a8c8e5457e
fmatheus <none@none>
2011-09-16 18:49:58 -0300

    Added the new crew members and usage example for changelog build.
    --HG--
    branch : 0.14

--- hg2cl.sh
+++ hg2cl.sh
@@ -1,3 +1,9 @@
+# Usage example:
+#
+# ./hg2cl.sh -r "sort(0.13.0::.-branch(0.12),-date)+sort(3888::0.12.0,-date)" . > x && mv x ChangeLog
+#
+# PS: 3888 was the last revision where ChangeLog was updated
+
 hg --quiet log --style=changelog $* | perl -pe '
 s/  braulio.*>$/  Braulio Barros de Oliveira  <brauliobo\@gebrproject.com>/i;
 s/  (ian|ianliu).*>$/  Ian Liu Rodrigues  <ian.liu\@gebrproject.com>/i;
@@ -8,5 +14,9 @@ s/  rodrigo.*>$/  Rodrigo Morelatto  <morelatto\@gebrproject.com>/i;
 s/  jorge.*>$/  Jorge Pizzolatto <jorge.pzt\@gebrproject.com>/i;
 s/  sobral.*>/  Gabriel Sobral <sobral\@gebrproject.com>/i;
 s/  davi.*>$/  Davi Clemente <davi.clemente\@gebrproject.com>/i;
-s/  (fabioaz|fabiosazevedo).*>$/  Fabio de Souza Azevedo  <fabioaz\@gebrproject.com>/' | tail -n +2
-echo
+s/  pris.*>$/  Priscila Moraes de Souza <pris.moraess\@gebrproject.com>/i;
+s/  giuliano.*>$/  Giuliano Roberto Pinheiro <giuliano\@gebrproject.com>/i;
+s/  (keiji|eric).*>$/  Eric Keiji <keiji.eric\@gebrproject.com>/i;
+s/  (fabioaz|fabiosazevedo).*>$/  Fabio de Souza Azevedo  <fabioaz\@gebrproject.com>/' \
+| perl -ne '(/^2/ && print "\n$_\n") || /^$/ || print' | tail -n +2
+echo | paste -s -d\\n - ChangeLog
