b27360f56c16447fafe40c8475e6f4fccb15faa0
fmatheus <none@none>
2012-01-11 16:45:14 -0200

    Update divide_flows integration test.

--- libgebr/geoxml/tests/test-flow.c
+++ libgebr/geoxml/tests/test-flow.c
@@ -669,7 +669,7 @@ void test_gebr_geoxml_flow_divide_flows (Fixture *fixture, gconstpointer data)
 
 
 	gchar *n1 = gebr_geoxml_program_control_get_n(control1, &step, &ini);
-	g_assert_cmpstr(n1, ==, "41");
+	g_assert_cmpstr(n1, ==, "40");
 	g_assert_cmpstr(ini, ==, "1");
 	g_assert_cmpstr(step, ==, "1");
 	g_free(ini);
@@ -677,21 +677,21 @@ void test_gebr_geoxml_flow_divide_flows (Fixture *fixture, gconstpointer data)
 
 	gchar *n2 = gebr_geoxml_program_control_get_n(control2, &step, &ini);
 	g_assert_cmpstr(n2, ==, "30");
-	g_assert_cmpstr(ini, ==, "42");
+	g_assert_cmpstr(ini, ==, "41");
 	g_assert_cmpstr(step, ==, "1");
 	g_free(ini);
 	g_free(step);
 
 	gchar *n3 = gebr_geoxml_program_control_get_n(control3, &step, &ini);
 	g_assert_cmpstr(n3, ==, "20");
-	g_assert_cmpstr(ini, ==, "72");
+	g_assert_cmpstr(ini, ==, "71");
 	g_assert_cmpstr(step, ==, "1");
 	g_free(ini);
 	g_free(step);
 
 	gchar *n4 = gebr_geoxml_program_control_get_n(control4, &step, &ini);
-	g_assert_cmpstr(n4, ==, "10");
-	g_assert_cmpstr(ini, ==, "92");
+	g_assert_cmpstr(n4, ==, "11");
+	g_assert_cmpstr(ini, ==, "91");
 	g_assert_cmpstr(step, ==, "1");
 	g_free(ini);
 	g_free(step);
