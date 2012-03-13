82295d2e8af25531549adca101dd84de1cbd5b32
Ian Liu Rodrigues <ian.liu88@gmail.com>
2012-03-13 14:50:00 -0300

    Add copyright notice to file

--- gebrd/gebrd-mpi-implementations.c
+++ gebrd/gebrd-mpi-implementations.c
@@ -1,4 +1,22 @@
-#include <string.h>
+/*
+ * gebrd-mpi-implementations.c
+ * This file is part of GêBR Project
+ *
+ * Copyright (C) 2012 - GêBR Core Team (www.gebrproject.com)
+ *
+ * GêBR Project is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * GêBR Project is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GêBR Project. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include "gebrd-mpi-implementations.h"
 
