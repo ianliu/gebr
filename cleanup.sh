c2d0e36d862cfb849d0dc07b19bbecbb6c2ace89
fmatheus@gebrproject.com <fmatheus@gebrproject.com>
2012-04-27 11:44:40 -0300

    Cleanup.sh handles authorized_keys.cleanup if it exists.

--- cleanup.sh
+++ cleanup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+AUTH="$HOME/.ssh/authorized_keys"
 shopt -s nullglob
 shopt -s extglob
 signal=${2--15}
@@ -8,7 +9,7 @@ echo Killing $1...
 for lock in $HOME/.gebr/$1/*/lock; do
     host=`basename ${lock%lock}`;
     echo -en "$host\t"
-    ssh $host fuser -sk $signal '$(cat '$lock')/tcp' \; killall -q mpich-hello
+    ssh $host killall -q mpich-hello \; fuser -sk $signal '$(cat '$lock')/tcp'
     if [ $? -eq 1 ]; then
 	echo -e "CRASH"; rm -rf $lock
     else
@@ -18,6 +19,12 @@ done
 }
 
 sync
+
+if [ -f $AUTH.cleanup ]; then
+	[ -f $AUTH ] && mv $AUTH $AUTH.cleanup.tmp
+	mv $AUTH.cleanup $AUTH
+	UNDO_AUTH=1
+fi
 
 case x$1 in
     xgebrd | xd)
@@ -39,3 +46,8 @@ case x$1 in
       [ -d ~/.gebr ] && mv ~/.gebr ~/.gebr-`date +%Y%m%d-%H%M%S`
       ;;
 esac
+
+if [ $UNDO_AUTH ]; then
+	mv $AUTH $AUTH.cleanup
+	[ -f $AUTH.cleanup.tmp ] && mv $AUTH.cleanup.tmp $AUTH
+fi
