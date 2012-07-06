3a1194662fb3ee199e205959821458be9f882657
Jorge Pizzolatto Filho <jorge.pzt@gebrproject.com>
2012-07-06 16:45:42 -0300

    Create treatment to double-click and create menu with rigth button
    --HG--
    branch : xdot

--- gebr/data/gebr-xdot-graph.py
+++ gebr/data/gebr-xdot-graph.py
@@ -7,12 +7,11 @@ class MyDotWindow(xdot.DotWindow):
     def __init__(self):
         xdot.DotWindow.__init__(self)
         self.widget.connect('clicked', self.on_url_clicked)
+        self.widget.connect('activate', self.on_url_activate)
 
         Wid = 0L
         if len(sys.argv) == 2:
             Wid = long(sys.argv[1])
-
-        print "Window ID is "+str(Wid)
 
         self.plug = gtk.Plug(Wid)
 
@@ -24,8 +23,35 @@ class MyDotWindow(xdot.DotWindow):
 
         self.window.destroy()
 
+    def on_revert_clicked(self, widget, url):
+        revert_str = "revert:"+url
+        sys.stderr.write(str(revert_str))
+        self.menu.destroy()
+
+    def on_delete_clicked(self, widget, url):
+        delete_str = "delete:"+url
+        sys.stderr.write(str(delete_str))
+        self.menu.destroy()
+    
     def on_url_clicked(self, widget, url, event):
-        sys.stderr.write(str(url))
+        if url == "head":
+            return False
+        self.menu = gtk.Menu()
+        self.menu.popup(None, None, None, event.button, event.time, url)
+        revert = gtk.MenuItem("Revert")
+        revert.connect("activate", self.on_revert_clicked, url)
+        revert.show()
+        self.menu.append(revert)
+        delete = gtk.MenuItem("Delete")
+        delete.connect("activate", self.on_delete_clicked, url)
+        delete.show()
+        self.menu.append(delete)
+        self.menu.show_all()
+        return True
+
+    def on_url_activate(self, widget, url, event):
+        revert_str = "revert:"+url
+        sys.stderr.write(str(revert_str))
         return True
 
     def create_dot_graph(self, fd, condition):
@@ -36,7 +62,7 @@ class MyDotWindow(xdot.DotWindow):
 
 def main():
     window = MyDotWindow()
-    
+
     glib.io_add_watch(sys.stdin, # FILE DESCRIPTOR
                       glib.IO_IN,  # CONDITION
                       window.create_dot_graph) # CALLBACK
