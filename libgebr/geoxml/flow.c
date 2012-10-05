72dc4f63dc6af983ab96c41c50c45b9107ef3283
Igor Nunes <igorsnunes@gebrproject.com>
2012-10-05 17:16:47 -0300

    Fixing sentence about programs with errors in case of one program with error

--- libgebr/geoxml/flow.c
+++ libgebr/geoxml/flow.c
@@ -911,7 +911,8 @@ gebr_geoxml_flow_validate(GebrGeoXmlFlow *flow,
 	if (progs_error > 0) {
 		g_set_error(err, GEBR_GEOXML_FLOW_ERROR,
 		            GEBR_GEOXML_FLOW_ERROR_PROGRAM,
-		            _("There are %d %s with error"),
+		            _("There %s %d %s with error"),
+		            progs_error == 1? "is": "are",
 		            progs_error, progs_error == 1? "program" : "programs");
 		gebr_geoxml_object_unref(seq);
 		gebr_pairstrfreev(pvector);
