11eb59e4d8a67ea370fa8616a1887e3e3afc9e3d
Ian Liu Rodrigues <ian.liu88@gmail.com>
2012-11-08 12:30:41 -0200

    libgebr: rename gebr_maestro_settings_add_address_on_domain to append_address
    --HG--
    branch : remove-local-ssh

--- libgebr/gebr-maestro-settings.h
+++ libgebr/gebr-maestro-settings.h
@@ -75,13 +75,20 @@ void gebr_maestro_settings_add_addresses_on_domain(GebrMaestroSettings *ms,
                                                    const gchar *addrs);
 
 /**
- * gebr_maestro_settings_add_address:
+ * gebr_maestro_settings_prepend_address:
+ */
+void gebr_maestro_settings_prepend_address(GebrMaestroSettings *ms,
+					   const gchar *domain,
+					   const gchar *addr);
+
+/**
+ * gebr_maestro_settings_append_address:
  *
  * Add @addr on list of maestro of keyfile, at @domain
  */
-void gebr_maestro_settings_add_address_on_domain(GebrMaestroSettings *ms,
-                                                 const gchar *domain,
-                                                 const gchar *addr);
+void gebr_maestro_settings_append_address(GebrMaestroSettings *ms,
+					  const gchar *domain,
+					  const gchar *addr);
 
 /**
  * gebr_maestro_settings_change_label:
