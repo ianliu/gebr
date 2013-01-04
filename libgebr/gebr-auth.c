138b1f8ac85a7049442f5147bc7c8f71529b3923
Ian Liu Rodrigues <ian.liu@gebrproject.com>
2013-01-04 15:24:34 -0200

    libgebr: add GebrAuth module
    --HG--
    branch : proxy-maestro

---
+++ libgebr/gebr-auth.c
@@ -0,0 +1,71 @@
+/*
+ * gebr-auth.c
+ * This file is part of GêBR Project
+ *
+ * Copyright (C) 2012 - GêBR Core team (www.gebrproject.com)
+ *
+ * GêBR Project is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * GêBR Project is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GêBR Project. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "gebr-auth.h"
+#include <stdio.h>
+
+#define COOKIE_LENGTH 64
+
+struct _GebrAuth {
+	GTree *cookies;
+};
+
+
+GebrAuth *
+gebr_auth_new(void)
+{
+	GebrAuth *auth = g_new0(GebrAuth, 1);
+	auth->cookies = g_tree_new_full((GCompareDataFunc)g_strcmp0,
+					NULL, g_free, NULL);
+	return auth;
+}
+
+void
+gebr_auth_read_cookie(GebrAuth *self)
+{
+	gchar *key = g_new(gchar, COOKIE_LENGTH);
+
+	if (scanf("%64s", key) != 1) {
+		g_warning("Could not retrieve cookie!");
+		g_free(key);
+		return;
+	}
+
+	g_tree_insert(self->cookies, key, GUINT_TO_POINTER(1));
+}
+
+gboolean
+gebr_auth_accepts(GebrAuth *self, const gchar *key)
+{
+	return g_tree_lookup_extended(self->cookies, key, NULL, NULL);
+}
+
+void
+gebr_auth_remove_cookie(GebrAuth *self, const gchar *key)
+{
+	g_tree_remove(self->cookies, key);
+}
+
+void
+gebr_auth_free(GebrAuth *self)
+{
+	g_tree_unref(self->cookies);
+	g_free(self);
+}
---
+++ libgebr/gebr-auth.h
@@ -0,0 +1,66 @@
+/*
+ * gebr-auth.h
+ * This file is part of GêBR Project
+ *
+ * Copyright (C) 2012 - GêBR Core team (www.gebrproject.com)
+ *
+ * GêBR Project is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * GêBR Project is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GêBR Project. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __GEBR_AUTH_H__
+#define __GEBR_AUTH_H__
+
+#include <glib.h>
+
+G_BEGIN_DECLS
+
+typedef struct _GebrAuth GebrAuth;
+
+/**
+ * gebr_auth_new:
+ *
+ * Creates a new GebrAuth object to handle a list of allowed cookies. See
+ * gebr_auth_accepts() method for validating a cookie.
+ */
+GebrAuth *gebr_auth_new(void);
+
+/**
+ * gebr_auth_read_cookie:
+ *
+ * Reads a cookie from the standard input and add it to the list of authorized
+ * cookies.
+ */
+void gebr_auth_read_cookie(GebrAuth *self);
+
+/**
+ * gebr_auth_accepts:
+ *
+ * Checks if @key is authorized. See gebr_auth_read_cookie() for authorizing
+ * cookies.
+ */
+gboolean gebr_auth_accepts(GebrAuth *self, const gchar *key);
+
+/**
+ * gebr_auth_remove_cookie:
+ *
+ * Removes @key from the authorized cookies.
+ */
+void gebr_auth_remove_cookie(GebrAuth *self, const gchar *key);
+
+void gebr_auth_free(GebrAuth *self);
+
+G_END_DECLS
+
+#endif /* end of include guard: __GEBR_AUTH_H__ */
+
